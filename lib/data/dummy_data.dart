00d4c6dd782b374215a4acafceb813d66ec0cdd8
Avinash174 <avinashmagar15@gmail.com>
2025-11-21 13:40:45 +0530

    new changes done

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/model/category.dart';
+
+final availablecategory = [
+  Category(id: 'c1', name: 'Italian', color: Colors.purple),
+  Category(id: 'c2', name: 'Quick & Easy', color: Colors.red),
+  Category(id: 'c3', name: 'Hamburgers', color: Colors.orange),
+  Category(id: 'c4', name: 'German', color: Colors.amber),
+  Category(id: 'c5', name: 'Light & Lovely', color: Colors.blue),
+];
---
+++ lib/widgets/categories_grid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/model/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [category.color.withOpacity(0.7), category.color],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Text(category.name, style: Theme.of(context).textTheme.titleLarge),
+    );
+  }
+}
